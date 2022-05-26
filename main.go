60e98185e8e8ba5d11c54d1e699118382bf115d0
Eko Kurniadi <eko.kurniadi@majoo.id>
2022-05-27 02:59:03 +0700

    add go version

--- main.go
+++ main.go
@@ -161,7 +161,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Credentials", "true")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT,DELETE")
-		c.Header("Access-Control-Allow-Headers:", "Authorization")
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
